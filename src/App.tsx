742772da684bd7b68075118d9f7364ccee0186a3
yunajoe <yunajoe@gmail.com>
2024-11-26 10:10:38 +0900

    feat: pagination으로 infinitescroll구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { getWorkers } from "./api";
 import "./App.css";
 import { ITEM } from "./type";
@@ -9,17 +9,55 @@ function App() {
   const [page, setPage] = useState(1);
   const [data, setData] = useState<[] | ITEM[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasNext, setHasNext] = useState(true);
+
+  const targetRef = useRef<null | HTMLDivElement>(null);
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+
+  const observerCallback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setPage((prev) => prev + 1);
+        }
+      });
+    },
+    []
+  );
 
   const fetchData = async () => {
     setIsLoading(true);
     const response = await getWorkers(page, LIMIT);
-    setData(response.data);
+    if (response.meta.totalCount === data.length) {
+      setHasNext(false);
+    }
+    if (data.length === 0) {
+      setData(response.data);
+    } else {
+      setData((prev) => [...prev, ...response.data]);
+    }
+
     setIsLoading(false);
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    const observer = new IntersectionObserver(observerCallback, options);
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+    return () => observer.disconnect();
+  }, [targetRef.current, observerCallback, options]);
+
+  useEffect(() => {
+    if (hasNext) {
+      fetchData();
+    }
+  }, [page]);
 
   if (isLoading) {
     return <h1>로딩중</h1>;
@@ -41,6 +79,7 @@ function App() {
           </div>
         );
       })}
+      <div ref={targetRef} style={{ margin: "10px" }}></div>
     </>
   );
 }
